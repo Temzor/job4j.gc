220c0925b2be51c0f2858124316d6d569edb666e
Temzor <onewaykd@gmail.com>
2022-01-17 17:25:00 +0300

    0. Виды ссылок [#6854]

--- src/main/java/ru/job4j/gc/reference/ExamWeakLinkDemo.java
+++ src/main/java/ru/job4j/gc/reference/ExamWeakLinkDemo.java
@@ -1,25 +1,41 @@
 package ru.job4j.gc.reference;
 
 import java.lang.ref.WeakReference;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 public class ExamWeakLinkDemo {
-    public static void main(String[] args) {
-        Object obj = new Object();
-        WeakReference<Object> weakobj
-                = new WeakReference<Object>(obj);
-        System.out.println(
-                "-> Retrieving object from weak reference using get ()");
-        System.out.println(weakobj.get());
-        System.out.println(
-                "-> Is it possible to queue object using enqueue ()");
-        System.out.println(weakobj.enqueue());
-        System.out.println(
-                "-> Checking if reference is queued using isEnqueued ()");
-        System.out.println(weakobj.isEnqueued());
+    public static void main(String[] args) throws InterruptedException {
+        example1();
+        example2();
     }
-}
-class X {
-    void show() {
-        System.out.println("show () from X invoked..");
+
+    private static void example1() throws InterruptedException {
+        Object object = new Object() {
+            @Override
+            protected void finalize() throws Throwable {
+                System.out.println("Removed");
+            }
+        };
+        WeakReference<Object> weak = new WeakReference<>(object);
+        object = null;
+        System.gc();
+        TimeUnit.SECONDS.sleep(3);
+        System.out.println(weak.get());
+    }
+
+    private static void example2() throws InterruptedException {
+        List<WeakReference<Object>> objects = new ArrayList<>();
+        for (int i = 0; i < 100; i++) {
+            objects.add(new WeakReference<Object>(new Object() {
+                @Override
+                protected void finalize() throws Throwable {
+                    System.out.println("Removed!");
+                }
+            }));
+        }
+        System.gc();
+        TimeUnit.SECONDS.sleep(3);
     }
 }
