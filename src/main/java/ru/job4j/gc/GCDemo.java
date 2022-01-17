98f2331d81d3543ce9431be135a0278d195cae40
Temzor <onewaykd@gmail.com>
2022-01-17 15:04:50 +0300

    0. Виды ссылок [#6854]

--- src/main/java/ru/job4j/gc/GCDemo.java
+++ src/main/java/ru/job4j/gc/GCDemo.java
@@ -1,5 +1,9 @@
 package ru.job4j.gc;
 
+import java.lang.ref.ReferenceQueue;
+import java.lang.ref.WeakReference;
+import java.util.concurrent.TimeUnit;
+
 public class GCDemo {
 
     private static final long KB = 1000;
@@ -23,6 +27,24 @@ public class GCDemo {
         }
         System.gc();
         info();
+    }
+
+    private static void example3() throws InterruptedException {
+        Object object = new Object() {
+            @Override
+            protected void finalize() {
+                System.out.println("Removed");
+            }
+        };
+        ReferenceQueue<Object> queue = new ReferenceQueue<>();
+        WeakReference<Object> weak = new WeakReference<>(object, queue);
+        object = null;
+
+        System.gc();
+
+        TimeUnit.SECONDS.sleep(3);
+        System.out.println("from link " + weak.get());
+        System.out.println("from queue " + queue.poll());
     }
 
 }
