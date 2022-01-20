1fe587da0b8ef5b619ff4c83f1ed338a0db78b72
Temzor <onewaykd@gmail.com>
2022-01-20 10:05:58 +0300

    1. Реализация кеша на SoftReference [#1592]

--- src/main/java/ru/job4j/cache/AbstractCache.java
+++ src/main/java/ru/job4j/cache/AbstractCache.java
@@ -9,21 +9,12 @@ public abstract class AbstractCache<K, V> {
     protected final Map<K, SoftReference<V>> cache = new HashMap<>();
 
     public void put(K key, V value) {
-        cache.putIfAbsent(key, new SoftReference<>(value));
 
     }
 
     public V get(K key) {
-        V result = null;
-        if (cache.containsKey(key)) {
-            if (cache.get(key).get() == null) {
-                cache.put(key, new SoftReference<>(load(key)));
-            }
-            result  = cache.get(key).get();
-        }
-        return result;
+       return null;
     }
 
     protected abstract V load(K key);
-
 }
